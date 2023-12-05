6125b61391d967c31df593530d76a968f9dab3f1
unknown <krisugergaming@gmail.com>
2023-12-05 14:54:10 +0200

    Added playlists display functionailty

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
@@ -13,6 +15,9 @@ class AuthController extends Controller
 
     function callback(){
         $user = Socialite::driver("spotify")->user();
+        DB::insert("insert into users(name, token) values(?,?)", [$user->name, $user->token]);
+        
+        
         dd($user);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -19,3 +21,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/get', function() {
+    $token = DB::select('select users.token from users where users.id=1');
+    $res = Http::withHeaders(['Authorization' => "Bearer " . $token[0]->token])->get('https://api.spotify.com/v1/me/playlists');
+    dd($res->body());
+});
